03f9207e30d8a93267ebc5dfb2a855fba76e0591
devlucascalado <devlucascalado@gmail.com>
2024-01-25 12:11:40 -0300

    exercicio01-sequencial

---
+++ exercicio01_sequencial/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+//1. Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Alo mundo!!");
+    }
+}
