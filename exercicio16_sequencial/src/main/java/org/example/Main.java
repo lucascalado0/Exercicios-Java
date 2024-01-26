ab3a99feef77f8772bd1e7a8d907084c5544b4d1
devlucascalado <devlucascalado@gmail.com>
2024-01-25 23:11:23 -0300

    exercicio16-sequencial

---
+++ exercicio16_sequencial/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+//16. Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros
+// quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada
+// 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00.
+// Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.
+
+package org.example;
+
+import java.util.Scanner; //importando ferramenta scanner que permite interação do usuário com o programa
+import java.util.Locale; //importando ferramenta locale que permite entrada de numeros reais no padrao us ex: "10.5"
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in).useLocale(Locale.US); //instanciando scanner + locale
+
+        int area;
+        double cobertura = 3;
+
+        System.out.println("Informe a área a ser pintada(em metros): ");
+        area = scanner.nextInt();
+
+        double quantidadeDeTinta = area / cobertura;
+        double quantidadeDeLatas = (int) Math.ceil(quantidadeDeTinta / 18);
+        double precoTotal = quantidadeDeLatas * 80.00;
+
+        System.out.println("Quantidade de latas necessárias: " + quantidadeDeLatas);
+        System.out.println("\nPreço total a ser pago: R$" + precoTotal);
+
+    }
+}
